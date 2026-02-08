d48a7776412660231e6d979f212dd4ef2037f973
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2026-02-08 15:43:30 +0530

    socket.io client connection tested

--- sockets/tile.socket.js
+++ sockets/tile.socket.js
@@ -2,12 +2,20 @@ require("./sockets/tile.socket")(io);
 
 import "./sockets/tile.socket";
 
-export default (io) => {
-  io.on("connection", (socket) => {
-    console.log("Tile user connected:", socket.id);
+// export default (io) => {
+//   io.on("connection", (socket) => {
+//     console.log("Tile user connected:", socket.id);
 
-    socket.on("claim_tile", (tileId) => {
-      // claim logic here later
-    });
+//     socket.on("claim_tile", (tileId) => {
+//       // claim logic here later
+//     });
+//   });
+// };
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("hello", (data) => {
+    console.log("Client says:", data);
   });
-};
+});
